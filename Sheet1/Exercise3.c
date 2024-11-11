95fb6d1f3bc629f40f93090449e0dca4fa74b234
Hilton Tavares <hilton.edeir@gmail.com>
2024-11-11 23:14:24 +0000

    update

---
+++ Sheet1/Exercise3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define PI 3.14
+
+// 3.Elabore um algoritmo que calcule a área e o perímetro de um círculo.
+
+int main() {
+    float raio, perimetro;
+
+    printf("Insira o raio do circulo: ");
+    scanf("%f", &raio);
+
+    perimetro = 2 * PI * raio;
+    printf("O perimetro do circulo com raio %.2f é de %.2f", raio, perimetro);
+
+    return 0;
+}
