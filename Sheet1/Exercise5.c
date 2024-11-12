bc51e9e28a5fd730d17104f8fe1d7a1fcb5974e9
Hilton Tavares <hilton.edeir@gmail.com>
2024-11-12 00:23:35 +0000

    update

--- Sheet1/Exercise5.c
+++ Sheet1/Exercise5.c
@@ -9,34 +9,65 @@ fixo e o valor recebido por carro vendido e calcula o salário mensal do vendedo
 juntamente com o seu número de identificação
 */
 
-int main(){
+int main() {
 
     int numero_vendedor;
     float salario_fixo;
+    float salario_comissao;
     int numero_carros_vendidos;
     float preco_carro;
-    float preco_total_venda = 0;
+    float preco_total_vendido = 0;
     float comissao_venda = 0;
 
     printf("*** SALARIO MENSAL DOS FUNCIONARIOS ***\n\n");
     
-    printf("Numero do vendedor: ");
-    scanf("%d", &numero_vendedor);
+    do {
+        printf("Numero do vendedor: ");
+        scanf("%d", &numero_vendedor);
 
-    printf("Salario fixo (Euros): ");
-    scanf("%f", &salario_fixo);
+        if(numero_vendedor <= 0)
+            printf("Numero de vendedor invalido!\n");
+    } while(numero_vendedor <= 0);
 
-    printf("Total de carros vendidos: ");
-    scanf("%d", &numero_carros_vendidos);
+    do {
+        printf("Salario fixo (Euros): ");
+        scanf("%f", &salario_fixo);
 
+        if(salario_fixo <= 0)
+            printf("Salario invalido\n");
+    } while(salario_fixo <= 0);
+    
+    do {
+        printf("Total de carro(s) vendido(s): ");
+        scanf("%d", &numero_carros_vendidos);
+
+        if(numero_carros_vendidos <= 0)
+            printf("Total de carro(s) vendido(s) invalido\n");
+    } while (numero_carros_vendidos <= 0);
+   
     for(int i = 1; i <= numero_carros_vendidos; i++) {
         do {
             printf("Preco do carro %d: ", i);
             scanf("%f", &preco_carro);
-        } while(preco_carro < 0);
 
-        preco_total_venda = preco_total_venda + preco_carro;
+            if (preco_carro <= 0) {
+                printf("Preco invalido!\n");  
+            }
+        } while(preco_carro <= 0);
+
+        preco_total_vendido = preco_total_vendido + preco_carro;
     }
+
+    comissao_venda = preco_total_vendido * 0.05;
+    salario_comissao = salario_fixo + comissao_venda;
+
+    printf("\n------------------------------------------");    
+    printf("\nFuncionario: %.8d", numero_vendedor);
+    printf("\nSalario fixo: %.2f Euros" , salario_fixo);
+    printf("\nTotal de carro(s) vendido(s): %d", numero_carros_vendidos);
+    printf("\nPreco total vendido: %.2f Euros", preco_total_vendido);
+    printf("\nComissao de venda ganhado: %.2f Euros", comissao_venda);
+    printf("\nSalario com comissao: %.2f Euros", salario_comissao);
 
     return 0;
 }
