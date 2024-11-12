15a2ff4f74d4c75d4fde573213eab25153242124
Hilton Tavares <hilton.edeir@gmail.com>
2024-11-12 03:15:00 +0000

    update

--- Sheet1/Exercise7.c
+++ Sheet1/Exercise7.c
@@ -10,9 +10,35 @@ Km/l e o lucro diário.
 
 int main() {
 
+    float preco_combustivel = 1.3;
+    float conta_km_inicial;
+    float conta_km_final;
+    float combustivel_gasto;
+    float faturacao_dia;
+    float lucro_diario;
+    float media_consumo;
+
+    printf("\n*** RENDIMENTO DO CARRO ***\n\n");
+
+    printf("Insira kilometragem incial: ");
+    scanf("%f", &conta_km_inicial);
+
+    printf("Insira kilometragem final: ");
+    scanf("%f", &conta_km_final);
+
+    printf("Combustivel gasto (litro): ");
+    scanf("%f", &combustivel_gasto);
 
 
-    printf("\n*** ***\n\n");
+    printf("Faturacao do dia (Eur): ");
+    scanf("%f", &faturacao_dia);
+    
+    media_consumo = (conta_km_final - conta_km_inicial) / combustivel_gasto;
+    lucro_diario = faturacao_dia - (combustivel_gasto * preco_combustivel);
+
+    printf("\n-----------------------------");
+    printf("\nMedia do consumo: %.2f km/l", media_consumo);
+    printf("\nLucro diario: %.2f Euros", lucro_diario);
 
     return 0;
 }
---
+++ Sheet1/Exercise9.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+9. Um agricultor efectuou a sua vindima e vai destinar 3/4 da sua colheita para produzir vinho numa
+cooperativa, ficando o restante para seu consumo pessoal. Da quantidade entregue na cooperativa
+3/5 serão utilizados para a produção de vinho branco e o restante para vinho tinto. Elabore um
+algoritmo que obtendo a quantidade (em Kg) de uvas colhidas, calcule as quantidades que vão ser
+destinadas a consumo próprio e à produção de cada um dos vinhos.
+*/
+
+int main(){
+
+    printf("*** ***\n\n");
+    return 0;
+}
